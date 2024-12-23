5cf4d6a1f8766bf3b7b4f908221380dae194b069
BartoszOlszak <bartosz.olszak47@o2.pl>
2024-12-23 12:17:23 +0100

    Add files via upload

---
+++ Wszystkie sortowania/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Wszystkie_sortowania
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            numericUpDown1 = new NumericUpDown();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(43, 371);
+            button1.Name = "button1";
+            button1.Size = new Size(108, 54);
+            button1.TabIndex = 0;
+            button1.Text = "BubbleSort";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(186, 371);
+            button2.Name = "button2";
+            button2.Size = new Size(108, 54);
+            button2.TabIndex = 1;
+            button2.Text = "InsertionSort";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(329, 371);
+            button3.Name = "button3";
+            button3.Size = new Size(108, 54);
+            button3.TabIndex = 2;
+            button3.Text = "MergeSort";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(477, 371);
+            button4.Name = "button4";
+            button4.Size = new Size(108, 54);
+            button4.TabIndex = 3;
+            button4.Text = "QuickSort";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(634, 371);
+            button5.Name = "button5";
+            button5.Size = new Size(108, 54);
+            button5.TabIndex = 4;
+            button5.Text = "CountingSort";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(43, 45);
+            button6.Name = "button6";
+            button6.Size = new Size(108, 54);
+            button6.TabIndex = 5;
+            button6.Text = "Generuj";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(319, 62);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(423, 23);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(186, 227);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(423, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(174, 62);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(51, 235);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 15);
+            label1.TabIndex = 9;
+            label1.Text = "Posortowana lista";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private NumericUpDown numericUpDown1;
+        private Label label1;
+    }
+}
