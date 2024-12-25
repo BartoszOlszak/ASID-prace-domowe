d8ba37987619bcf78e75a0e0f9b29c6b844963b0
BartoszOlszak <bartosz.olszak47@o2.pl>
2024-12-25 12:23:07 +0100

    Add files via upload

---
+++ NodeList/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeList
+{
+    internal class Node
+    {
+        public Node next;
+        public Node prev;
+        public int data;
+
+        public Node(int data)
+        {
+            this.next = null;
+            this.prev = null;
+            this.data = data;
+        }
+
+        public String NodeToString()
+        {
+            return data.ToString();
+        }
+
+    }
+}
